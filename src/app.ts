708832a00751bb9b12333ac5d5e22be94c83517c
Anik Islam <islammoshiu@gmail.com>
2026-05-22 16:55:25 +0600

    issue route import on app.js

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,14 @@ import express, { type Request, type Response } from "express";
 import cors from "cors";
 
 import authRoutes from "./module/auth/auth.routes";
-
+import issueRoutes from "./module/issues/issue.routes";
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
-
+app.use("/api/issues", issueRoutes);
 app.get("/", (req: Request, res: Response) => {
   res.send("DevPulse Server Running");
 });
