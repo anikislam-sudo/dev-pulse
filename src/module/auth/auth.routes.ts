bf3df6c561f1e29aca1f09ddd89199f5625e00a2
Anik Islam <islammoshiu@gmail.com>
2026-05-22 16:20:11 +0600

    auth route add

---
+++ src/module/auth/auth.routes.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+import { signupUser, loginUser } from "./auth.controller";
+
+const router = express.Router();
+
+router.post("/signup", signupUser);
+router.post("/login", loginUser);
+
+export default router;
